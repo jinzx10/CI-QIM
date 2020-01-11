88ea514da567efb9f6ab3268ccdf1c020265e87c
jinzx10 <jzx016@hotmail.com>
2020-01-10 19:27:54 -0500

    hw

--- src/gauss.cpp
+++ src/gauss.cpp
@@ -9,6 +9,8 @@ double gauss(double const& x, double const& mu, double const& sigma) {
 
 mat gauss(vec const& x, rowvec const& y, double const& sigma) {
 	return 1.0 / sigma / sqrt(2.0 * datum::pi) * 
-		exp( -square( repmat(x, 1, y.n_elem) - repmat(y, x.n_elem, 1) ) /
+		exp( -square( x*ones(1, y.n_elem) - ones(x.n_elem, 1)*y ) /
 				( 2.0 * sigma * sigma ) );
+//		exp( -square( repmat(x, 1, y.n_elem) - repmat(y, x.n_elem, 1) ) /
+//				( 2.0 * sigma * sigma ) );
 }
