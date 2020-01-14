874a6a1ca90e5705336913a357dce24abdf84b43
jinzx10 <jzx016@hotmail.com>
2020-01-13 22:57:13 -0500

    hw

--- src/fermi.cpp
+++ src/fermi.cpp
@@ -3,15 +3,9 @@
 using namespace arma;
 
 double fermi(double const& E, double const& mu, double const& kT) {
-	if ( std::abs(kT) < datum::eps )
-		return (E < mu);
-	else
-		return 1.0 / ( std::exp( (E - mu) / kT ) + 1.0 );
+	return ( std::abs(kT) < datum::eps ) ?  (E < mu) : 1.0 / ( std::exp( (E - mu) / kT ) + 1.0 );
 }
 
 vec fermi(vec const& E, double const& mu, double const& kT) {
-	if ( std::abs(kT) < datum::eps )
-		return conv_to<vec>::from(E < mu);
-	else
-		return 1.0 / ( exp( (E - mu) / kT ) + 1.0 );
+	return ( std::abs(kT) < datum::eps ) ?  conv_to<vec>::from(E < mu) : 1.0 / ( exp( (E - mu) / kT ) + 1.0 );
 }
