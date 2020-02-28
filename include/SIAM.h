bf5b2aa72ea79498083096fdbdb912578efcd4b2
jinzx10 <jzx016@hotmail.com>
2020-02-28 18:00:13 -0500

    hw

--- include/SIAM.h
+++ include/SIAM.h
@@ -9,25 +9,25 @@ struct SIAM
 	using d2d = std::function<double(double)>;
 
 	SIAM(
-			d2d		Ed_,
-			d2d		E_nuc_,
-			arma::vec const& bath_,
-			arma::vec const& cpl_,
-			double const& U_,
-			arma::uword const& n_occ_
+			d2d							Ed_,
+			d2d							E_nuc_,
+			arma::vec		const&		bath_,
+			arma::vec 		const& 		cpl_,
+			double			const&		U_,
+			arma::uword 	const& 		n_occ_
 	);
 
-	void set_and_calc(double const& x_);
-	double x;
+	void				set_and_calc(double const& x_);
+	double				x;
 
-	void solve_mf();
-	double n_mf;
-	double E_mf;
-	arma::mat vec_mf;
-	arma::vec val_mf;
+	void				solve_mf();
+	double				n_mf;
+	double				E_mf;
+	arma::mat			vec_mf;
+	arma::vec 			val_mf;
 
-	void rotate_orb();
-	void subrotate(arma::mat const& vec_sub, double& val_d, arma::vec& vec_d, arma::mat& vec_other, arma::sp_mat& H_other, arma::mat& H_d_other);
+	void				rotate_orb();
+	void				subrotate(arma::mat const& vec_sub, double& val_d, arma::vec& vec_d, arma::mat& vec_other, arma::sp_mat& H_other, arma::mat& H_d_other);
 	// o(v) stands for occupied (virtual) except do(dv)
 	double				val_do;
 	double 				val_dv;
@@ -40,26 +40,30 @@ struct SIAM
 	arma::mat			H_do_o;
 	arma::mat 			H_dv_v;
 
-	void solve_cisnd();
+	void				solve_cisnd();
+	arma::vec			val_cisnd;
+	arma::mat 			vec_cisnd;
 
-	void calc_bath();
-	void calc_Gamma();
-	void calc_dc();
+	void				calc_bath();
 
-	d2d Ed;
-	d2d E_nuc;
-	arma::vec bath;
-	arma::vec cpl;
-	arma::sp_mat h; // one-body part of the Hamiltonian
-	double U;
-	arma::uword n_bath;
-	arma::uword n_occ;
-	arma::uword n_vir;
-	arma::span span_occ;
-	arma::span span_vir;
-	arma::sp_mat F(double const& n);
-	arma::sp_mat F();
-	double n2n(double const& n);
+	void 				calc_Gamma();
+
+	void 				calc_dc();
+
+	d2d					Ed;
+	d2d 				E_nuc;
+	arma::vec			bath;
+	arma::vec 			cpl;
+	arma::sp_mat		h; // one-body part of the Hamiltonian
+	double				U;
+	arma::uword			n_bath;
+	arma::uword 		n_occ;
+	arma::uword 		n_vir;
+	arma::span			span_occ;
+	arma::span 			span_vir;
+	arma::sp_mat		F(double const& n);
+	arma::sp_mat 		F();
+	double				n2n(double const& n);
 
 
 	// data for the last position, used to calculate force and dc
@@ -71,6 +75,94 @@ struct SIAM
 	arma::vec			_vec_dv;
 	arma::mat			_vec_o;
 	arma::mat			_vec_v;
+
+	// matrix elements
+	arma::mat			Pdodv();
+	arma::mat			Pdv();
+	arma::mat			Pdo();
+	arma::mat			Pdob();
+	arma::mat			Pjdv();
+	arma::mat			Pdvb();
+	arma::mat			Pjdo();
+	arma::mat			Pab();
+	arma::mat			Pji();
+	
+	arma::mat			Fdo();
+	arma::mat			Fdv();
+	arma::mat			Fo();
+	arma::mat			Fv();
+	arma::mat			Fdvb();
+	arma::mat			Fjdo();
+
+	arma::mat			H_gnd_gnd();
+	arma::mat			H_gnd_dodv();
+	arma::mat			H_gnd_dob();
+	arma::mat			H_gnd_jdv();
+	arma::mat			H_gnd_ovov();
+	arma::mat			H_gnd_ovob();
+	arma::mat			H_gnd_ovjv();
+
+	arma::mat			H_dodv_dodv();
+	arma::mat			H_dodv_dob();
+	arma::mat			H_dodv_jdv();
+	arma::mat			H_dodv_ovov();
+	arma::mat			H_dodv_ovob();
+	arma::mat			H_dodv_ovjv();
+
+	arma::mat			H_doa_dob();
+	arma::mat			H_doa_jdv();
+	arma::mat			H_doa_ovov();
+	arma::mat			H_doa_ovob();
+	arma::mat			H_doa_ovjv();
+
+	arma::mat			H_idv_jdv();
+	arma::mat			H_idv_ovov();
+	arma::mat			H_idv_ovob();
+	arma::mat			H_idv_ovjv();
+
+	arma::mat			H_ovov_ovov();
+	arma::mat			H_ovov_ovob();
+	arma::mat			H_ovov_ovjv();
+
+	arma::mat			H_ovoa_ovob();
+	arma::mat			H_ovoa_ovjv();
+
+	arma::mat			H_oviv_ovjv();
+
+	arma::mat			n_gnd_gnd();
+	arma::mat			n_gnd_dodv();
+	arma::mat			n_gnd_dob();
+	arma::mat			n_gnd_jdv();
+	arma::mat			n_gnd_ovov();
+	arma::mat			n_gnd_ovob();
+	arma::mat			n_gnd_ovjv();
+
+	arma::mat			n_dodv_dodv();
+	arma::mat			n_dodv_dob();
+	arma::mat			n_dodv_jdv();
+	arma::mat			n_dodv_ovov();
+	arma::mat			n_dodv_ovob();
+	arma::mat			n_dodv_ovjv();
+
+	arma::mat			n_doa_dob();
+	arma::mat			n_doa_jdv();
+	arma::mat			n_doa_ovov();
+	arma::mat			n_doa_ovob();
+	arma::mat			n_doa_ovjv();
+
+	arma::mat			n_idv_jdv();
+	arma::mat			n_idv_ovov();
+	arma::mat			n_idv_ovob();
+	arma::mat			n_idv_ovjv();
+
+	arma::mat			n_ovov_ovov();
+	arma::mat			n_ovov_ovob();
+	arma::mat			n_ovov_ovjv();
+
+	arma::mat			n_ovoa_ovob();
+	arma::mat			n_ovoa_ovjv();
+
+	arma::mat			n_oviv_ovjv();
 };
 
 
--- src/SIAM.cpp
+++ src/SIAM.cpp
@@ -82,6 +82,193 @@ void SIAM::subrotate(mat const& vec_sub, double& val_d, vec& vec_d, mat& vec_oth
 	H_d_other= vec_d.t() * F() * vec_other;
 }
 
+mat SIAM::Pdodv() {
+	return mat{vec_do(0) * vec_dv(0)};
+}
+
+mat SIAM::Pdv() {
+	return mat{vec_dv(0) * vec_dv(0)};
+}
+
+mat SIAM::Pdo() {
+	return mat{vec_do(0) * vec_do(0)};
+}
+
+mat SIAM::Pdob() {
+	return vec_do(0) * vec_v.row(0);
+}
+
+mat SIAM::Pjdv() {
+	return vec_dv(0) * vec_o.row(0);
+}
+
+mat SIAM::Pdvb() {
+	return vec_dv(0) * vec_v.row(0);
+}
+
+mat SIAM::Pjdo() {
+	return vec_do(0) * vec_o.row(0);
+}
+
+mat SIAM::Pab() {
+    return vec_v.row(0).t() * vec_v.row(0);
+}
+
+mat SIAM::Pji() {
+    return vec_o.row(0).t() * vec_o.row(0);
+}
+
+mat SIAM::Fdo() {
+	return vec_do.t() * F() * vec_do;
+}
+
+mat SIAM::Fdv() {
+	return vec_dv.t() * F() * vec_dv;
+}
+
+mat SIAM::Fo() {
+	return vec_o.t() * F() * vec_o;
+}
+
+mat SIAM::Fv() {
+	return vec_v.t() * F() * vec_v;
+}
+
+mat SIAM::Fdvb() {
+	return vec_dv.t() * F() * vec_v;
+}
+
+mat SIAM::Fjdo() {
+	return -vec_do.t() * F() * vec_o;
+}
+
+mat SIAM::H_gnd_gnd() {
+	return mat{E_mf};
+}
+
+mat SIAM::H_gnd_dodv() {
+	return mat{0};
+}
+
+mat SIAM::H_gnd_dob() {
+	return zeros(1, n_vir-1);
+}
+
+mat SIAM::H_gnd_jdv() {
+	return zeros(1, n_occ-1);
+}
+
+mat SIAM::H_gnd_ovov() {
+	return U * Pdodv() * Pdodv();
+}
+
+mat SIAM::H_gnd_ovob() {
+   	return sqrt(2) * U * Pdodv() * Pdob();
+}
+
+mat SIAM::H_gnd_ovjv() {
+    return sqrt(2) * U * Pdodv() * Pjdv();
+}
+
+mat SIAM::H_dodv_dodv() {
+	return E_mf + Fdv() - Fdo() + U * Pdodv() * Pdodv();
+}
+
+mat SIAM::H_dodv_dob() {
+    return Fdvb() + U * Pdodv() * Pdob();
+}
+
+mat SIAM::H_dodv_jdv() {
+	return -Fjdo() + U * Pdodv() * Pjdv();
+}
+
+mat SIAM::H_dodv_ovov() {
+   	return sqrt(2.0) * U * Pdodv() * (Pdv()-Pdo());
+}
+
+mat SIAM::H_dodv_ovob() {
+	return U * Pdodv() * (Pdv()-Pdo()) * Pdob() + U * Pdodv() * Pdvb();
+}
+
+mat SIAM::H_dodv_ovjv() {
+   	return U * Pdodv() * (Pdv()-Pdo()) * Pjdv() - U * Pdodv() * Pjdo();
+}
+
+mat SIAM::H_doa_dob() {
+	return Iv*(E0-Fdo) + Fv + U * (Pdob'*Pdob);
+}
+
+void SIAM::solve_cisnd() {
+    
+    H_doa_jdv = U * Pdob' * Pjdv;
+    H_doa_ovov = sqrt(2) * U * Pdvb' * Pdodv;
+    H_doa_ovob = U * (Pdvb'*Pdob + Pdodv*Pab - Iv*Pdodv*Pdo);
+    H_doa_ovjv = U * Pdvb' * Pjdv;
+    
+    H_idv_jdv = Io*(E0+Fdv) - Fo + U * (Pjdv' * Pjdv);
+    H_idv_ovov = -sqrt(2) * U * Pdodv * Pjdo';
+    H_idv_ovob = -U * Pjdo' * Pdob;
+    H_idv_ovjv = U * (-Pjdv'*Pjdo - Pdodv*Pji + Io*Pdodv*Pdv);
+    
+    
+    H_ovov_ovov = E0 + 2*(Fdv-Fdo) + U * (Pdv-Pdo)^2;
+    H_ovov_ovob = sqrt(2) * (Fdvb + U * (Pdv-Pdo) * Pdvb);
+    H_ovov_ovjv = -sqrt(2) * (Fjdo + U * (Pdv-Pdo) * Pjdo);
+    
+    H_ovoa_ovob = Iv*(E0+Fdv-Fdo) - Iv*(Fdo+U*(Pdv-Pdo)*Pdo) + Fv + U*(Pdv-Pdo)*Pab + U*(Pdvb'*Pdvb);
+    H_ovoa_ovjv = -U * Pdvb' * Pjdo;
+    
+    H_oviv_ovjv = Io*(E0+Fdv-Fdo) + Io*(Fdv+U*(Pdv-Pdo)*Pdv) - Fo - U*(Pdv-Pdo)*Pji + U*(Pjdo'*Pjdo);
+
+    
+    n_gnd_gnd = n_imp;
+    n_gnd_dodv = Pdodv/sqrt(2);
+    n_gnd_dob = Pdob/sqrt(2);
+    n_gnd_jdv = Pjdv/sqrt(2);
+    n_gnd_ovov = 0;
+    n_gnd_ovob = sparse(1, n_vir-1);
+    n_gnd_ovjv = sparse(1, n_occ-1);
+    
+    n_dodv_dodv = n_imp + 0.5*(Pdv-Pdo);
+    n_dodv_dob = 0.5*Pdvb;
+    n_dodv_jdv = -0.5*Pjdo;
+    n_dodv_ovov = Pdodv / sqrt(2);
+    n_dodv_ovob = 0.5*Pdob;
+    n_dodv_ovjv = 0.5*Pjdv;
+    
+    n_doa_dob = Iv*n_imp + 0.5*(Pab-Iv*Pdo);
+    n_doa_jdv = sparse(n_vir-1, n_occ-1);
+    n_doa_ovov = sparse(n_vir-1, 1);
+    n_doa_ovob = 0.5*Iv*Pdodv;
+    n_doa_ovjv = sparse(n_vir-1, n_occ-1);
+    
+    n_idv_jdv = Io*n_imp + 0.5*(Io*Pdv-Pji);
+    n_idv_ovov = sparse(n_occ-1, 1);
+    n_idv_ovob = sparse(n_occ-1, n_vir-1);
+    n_idv_ovjv = 0.5*Io*Pdodv;
+    
+    n_ovov_ovov = n_imp + Pdv - Pdo;
+    n_ovov_ovob = Pdvb / sqrt(2);
+    n_ovov_ovjv = -Pjdo / sqrt(2);
+    
+    n_ovoa_ovob = Iv*(n_imp-Pdo) + 0.5*(Pab+Iv*Pdv);
+    n_ovoa_ovjv = sparse(n_vir-1, n_occ-1);
+    
+    n_oviv_ovjv = Io*(n_imp+Pdv) - 0.5*(Pji+Io*Pdo);
+}
+
+void SIAM::calc_bath() {
+
+}
+
+void SIAM::calc_Gamma() {
+
+}
+
+void SIAM::calc_dc() {
+
+}
+
 
 void SIAM::move_new_to_old() {
 	_x = x;
