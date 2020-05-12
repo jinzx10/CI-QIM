09dbc769fd07152392ff0fc966c7b36a7955298e
jinzx10 <jzx016@hotmail.com>
2020-05-11 21:11:29 -0400

    hw

--- TwoPara.sh
+++ TwoPara.sh
@@ -2,10 +2,10 @@
 
 #PBS -N run_TwoPara
 #PBS -l nodes=1:ppn=48
-#PBS -q n3
+#PBS -q n7
 #PBS -l walltime=24:00:00
 #PBS -j oe
-#PBS -o /home/jinzx10/job/CI-QIM/log/TwoPara-n3.out
+#PBS -o /home/jinzx10/job/CI-QIM/log/TwoPara-n7.out
 
 JOBROOT=/home/jinzx10/job/CI-QIM/
 cd ${JOBROOT}
@@ -15,7 +15,7 @@ HYBRID=(   0.0128  0.0064  0.0032  0.0016  0.0008  0.0004  0.0002  0.0001  0.000
 DOX_PEAK=( 20      30      40      60      100     140     200     300     450      700)
 DOX_WIDTH=(5       4       3       2       1.4     1       0.7     0.5     0.3      0.2)
 
-for i in {7..7}
+for i in {8..8}
 do
 	SAVEDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}
 	mkdir -p ${SAVEDIR}
@@ -32,5 +32,5 @@ do
 	touch ${SAVEDIR}/run.log
 	echo $(date) >> ${SAVEDIR}/run.log
 
-	mpirun -np 24 ${JOBROOT}/bin/run_TwoPara ${SAVEDIR}/TwoPara.in >> ${SAVEDIR}/run.log
+	mpirun -np 24 ${JOBROOT}/bin/run_TwoPara ${SAVEDIR}/TwoPara.in >> ${SAVEDIR}/run.log 2>&1
 done
