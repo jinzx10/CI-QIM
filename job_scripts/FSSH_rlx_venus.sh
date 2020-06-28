acabe8da6659abb4a761edc5ca26c375ddcc679a
jinzx10 <jzx016@hotmail.com>
2020-06-27 23:35:42 -0400

    hw

--- job_scripts/FSSH_rlx_venus.sh
+++ job_scripts/FSSH_rlx_venus.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 #PBS -N run_FSSH_rlx
+##PBS -q n8
 #PBS -l nodes=5:ppn=48
-#PBS -l walltime=24:00:00
+#PBS -l walltime=168:00:00
 #PBS -j oe
 #PBS -o /home/jinzx10/job/CI-QIM/log/FSSH_rlx.out
 
 JOBROOT=/home/jinzx10/job/CI-QIM/
 cd $JOBROOT
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+LOG="run_fssh_rlx.${TIMESTAMP}.log"
 
 HYBRID=(	 0.0128  0.0064  0.0032  0.0016  0.0008  0.0004 0.0002  0.0001  0.00005  0.000025)
 T_MAX_LIST=( 1.5e5   3e5     5e5     1e6    1.4e6    2e6    3e6     5e6      1e7     1.5e7   )
-DTC_LIST=(   30      30      20      20      15      15     10      10       5       5)
-NUM_TRAJS=2000
-LOG="run_fssh_rlx.log"
+DTC_LIST=(   30      30      20      20      15      15     10      10       10      10)
+NUM_TRAJS=2400
 SZ_ELEC=30
 VELO_REV=0
+HAS_RLX=0
 
-for i in {0..0}
+for i in {5..9}
 do
 	READDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}
-	SAVEDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}/vr0_2
+	SAVEDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}/vr${VELO_REV}_rlx${HAS_RLX}/
 	mkdir -p ${SAVEDIR}
-	rm -f ${SAVEDIR}/${LOG}
 
 	cp ${JOBROOT}/tmp/FSSH_rlx_raw.in ${SAVEDIR}/FSSH_rlx.in
 
@@ -33,6 +35,7 @@ do
 		-e "s/DTC/${DTC_LIST[i]}/" \
 		-e "s/VELO_REV/${VELO_REV}/" \
 		-e "s/SZ_ELEC/${SZ_ELEC}/" \
+		-e "s/HAS_RLX/${HAS_RLX}/" \
 		${SAVEDIR}/FSSH_rlx.in
 
 	touch ${SAVEDIR}/${LOG}
--- job_scripts/TwoPara_venus.sh
+++ job_scripts/TwoPara_venus.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 #PBS -N run_TwoPara
-#PBS -l nodes=5:ppn=12
-#PBS -l mem=400gb
+#PBS -l nodes=4:ppn=24
+#PBS -l mem=380gb
 #PBS -q batch
 #PBS -l walltime=168:00:00
 #PBS -j oe
 #PBS -o /home/jinzx10/job/CI-QIM/log/TwoPara.out
 
 JOBROOT=/home/jinzx10/job/CI-QIM/
-LOG="run_TwoPara.log"
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+LOG="run_TwoPara.${TIMESTAMP}.log"
+
 cd ${JOBROOT}
 
-DOS_BASE=( 1000    1000	   2000    4000    8000    16000   16000   16000   16000    24000)
+DOS_BASE=( 1000    1000	   2000    4000    8000    16000   24000   24000   24000    24000)
 HYBRID=(   0.0128  0.0064  0.0032  0.0016  0.0008  0.0004  0.0002  0.0001  0.00005  0.000025)
 DOX_PEAK=( 20      30      40      60      100     140     200     300     450      700)
 DOX_WIDTH=(5       4       3       2       1.4     1       0.7     0.5     0.3      0.2)
 
-for i in {9..9}
+for i in {6..9}
 do
-	SAVEDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}_more
+	SAVEDIR=${JOBROOT}/data/TwoPara/hybrid_Gamma/${HYBRID[i]}_new
 	mkdir -p ${SAVEDIR}
-	rm -f ${SAVEDIR}/*.dat ${SAVEDIR}/*.txt ${SAVEDIR}/*.log
 
 	cp ${JOBROOT}/tmp/TwoPara_raw.in ${SAVEDIR}/TwoPara.in
 	sed -i -e "s@SAVEDIR@${SAVEDIR}@" \
@@ -34,5 +35,5 @@ do
 	touch ${SAVEDIR}/${LOG}
 	echo $(date) >> ${SAVEDIR}/${LOG}
 
-	mpirun -np 60 ${JOBROOT}/bin/run_TwoPara ${SAVEDIR}/TwoPara.in >> ${SAVEDIR}/${LOG} 2>&1
+	mpirun -np 96 ${JOBROOT}/bin/run_TwoPara ${SAVEDIR}/TwoPara.in >> ${SAVEDIR}/${LOG} 2>&1
 done
